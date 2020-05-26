2c19c01e71f9f8ece42451b61ca0ff56a4d62763
Fredrik Anderson <fredrik.andersson.2@volvo.com>
2020-05-26 10:44:31 +0200

    Bumping jackson versions.
    Adding jpaobjectgraph module
    Cleanup

---
+++ jpaobjectgraph/src/main/java/com/fredrik/jpaobjectgraph/JpaobjectgraphApplication.java
@@ -0,0 +1,15 @@
+package com.fredrik.jpaobjectgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaobjectgraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaobjectgraphApplication.class, args);
+		
+	}
+
+	
+}
---
+++ jpaobjectgraph/src/test/java/com/fredrik/jpaobjectgraph/JpaobjectgraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredrik.jpaobjectgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaobjectgraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
